1da55dfba1e83a7031ca027fb9c33570123c2ea8
back and white <01backandwhite@gmail.com>
2026-03-22 14:47:51 +0100

    Configurando la url del gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   enableMockFallback: false,
   apiBaseUrl: 'https://auth-service-des.up.railway.app/api/v1',
-  gatewayApiUrl: 'https://auth-service-des.up.railway.app/api/v1',
+  gatewayApiUrl: 'https://gateway-service-des.up.railway.app/api/v1',
 
   oauth2AuthorizeUrl: 'https://auth-service-des.up.railway.app/oauth2/authorize?',
   oauth2LoginUrl: 'https://auth-service-des.up.railway.app/login',
